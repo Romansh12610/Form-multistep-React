eaeea3dfe7c5f4ae5eec56d10fa8f8c7fab201f8
RomanSh <romashmarov82@gmail.com>
2023-11-08 04:21:26 +0300

    style update #1

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import FirstStep from './FirstStep';
+import './MainSection.scss';
 
 export default function Form( { step } ) {
     return (
-        <section className='main__section'>
-            <form className='main__form'>
+        <section className='main-section'>
+            <form className='main-section__form'>
                 <FirstStep />
             </form>
         </section>
