e2b46577f2362f6dfd8ba5b956f366b28356d34e
RomanSh <romashmarov82@gmail.com>
2023-11-20 05:06:21 +0300

    dasd

--- src/ConfirmChanges.jsx
+++ src/ConfirmChanges.jsx
@@ -1,52 +1,35 @@
 import { StepSetterContext } from "./App";
 import arrowSvg from './assets/images/arrow-back.svg';
 import './ConfirmChanges.scss';
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useRef, useState } from "react";
 
-export default function ConfirmChanges({ setIsConfirm, isMakingChanges }) {
-    //context
+export default function ConfirmChanges({ className, setIsConfirm, setIsMakeChanges }) {
+    // context & state
     const setStep = useContext(StepSetterContext);
-
-    //refs
+    const [showThanks, setShowThanks] = useState(false);
+    // ref
     const popupRef = useRef(null);
-    const buttonPopupRef = useRef(null);
-
-    useEffect(() => {
-        popupRef.current.classList.add('animated');
-        buttonPopupRef.current.classList.add('animated');
-        return () => {
-            popupRef.current.classList.remove('animated');
-            buttonPopupRef.current.classList.remove('animated');
-        }
-    }, []);
-
-    // handle confirm click
-    let showThanks = false;
+    // render code
     function handleConfirmClick() {
-        showThanks = true;
+        setShowThanks(true); 
         setTimeout(() => {
             setStep(3);
+            setIsMakeChanges(false);
             setIsConfirm(false);
-            isMakingChanges(false);
         }, 2500);
     }
 
     return (
         <div aria-live="polite"
-            className="confirm-popup"
+            className={className}
+            onClick={handleConfirmClick}
             ref={popupRef}
         >
             <p className="confirm-popup__p">Accept changes and go back to the confirming step?</p>
-            <button 
-                className="confirm-popup__button"
-                ref={buttonPopupRef}
-                onClick={handleConfirmClick}
-            >
-                <img 
-                    src={arrowSvg}
-                    className="confirm-popup__img"
-                />
-            </button>
+            <img 
+                src={arrowSvg}
+                className="confirm-popup__img"
+            />
             {showThanks && <p className="confirm-popup__thanks">
                 Thank you, for getting correct data to us!
             </p>}
