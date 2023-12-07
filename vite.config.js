8bb4fc47dd5fe5ae25f0b55a2ae01c41302dc09c
RomanSh <romashmarov82@gmail.com>
2023-12-07 04:19:15 +0300

    deploy #2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://Romansh12610.github.io/Form-multistep-React"
+  base: "/Form-multistep-React/"
 })
